096951709aedfe7513ab58349b8a3ec3f98b0559
sk-ys <yskrmt@gmail.com>
2023-07-03 23:16:51 +0900

    rails generate redmine_plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_note_list do
+  name 'Redmine Issue Note List plugin'
+  author 'sk-ys'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/sk-ys/redmine_issue_note_list'
+  author_url 'https://github.com/sk-ys'
+end
