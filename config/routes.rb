4233f345ccfd79938df26a4fa7ae758cb905706b
sk-ys <yskrmt@gmail.com>
2023-07-04 08:29:12 +0900

    Add a form to add a note #6

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :projects do
     resources :issue_notes, only: [:index]
   end
+
+  post '/issue_notes/:issue_id/add_note', to: 'issue_notes#add_note'
 end
