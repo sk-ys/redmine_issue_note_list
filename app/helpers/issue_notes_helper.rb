b50e1f5446580bc55441779b9d3b60e42a99b7bf
sk-ys <yskrmt@gmail.com>
2023-07-04 08:29:12 +0900

    Add a form to add a note #6

--- app/helpers/issue_notes_helper.rb
+++ app/helpers/issue_notes_helper.rb
@@ -58,4 +58,28 @@ module IssueNotesHelper
       issue_notes_path(*args)
     end
   end
+
+  def render_issue_note(issue, journal)
+    project = issue.project
+
+    content = +''
+    content << "<div class=\"issue-note\">"
+    content <<   "<h4 class=\"note-header\">"
+    content <<     link_to(
+                    format_time(journal.updated_on),
+                    @project.present? ?
+                    project_activity_path(@project, :from => User.current.time_to_date(journal.updated_on)) :
+                    activity_path( :from => User.current.time_to_date(journal.updated_on))
+                  )
+    content <<     render_private_notes_indicator(journal)
+    content <<   "</h4>"
+    content <<   "<div class=\"note-info\">"
+    content <<     l(journal.created_on == journal.updated_on ? :field_author : :field_updated_by).html_safe +
+                    ": " + link_to_user(journal.user)
+    content <<   "</div>"
+    content <<   render_notes(issue, journal)
+    content << "</div>"
+
+    content.html_safe
+  end
 end
