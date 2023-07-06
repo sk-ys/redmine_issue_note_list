5d095072c06470d1e980970449cee50c7125a192
sk-ys <yskrmt@gmail.com>
2023-07-06 16:31:02 +0900

    Change class .issue-note to .journal.has-notes

--- app/helpers/issue_notes_helper.rb
+++ app/helpers/issue_notes_helper.rb
@@ -63,7 +63,7 @@ module IssueNotesHelper
     project = issue.project
 
     content = +''
-    content << "<div class=\"issue-note\">"
+    content << "<div class=\"journal has-notes\">"
     content <<   "<h4 class=\"note-header\">"
     content <<     "<div class=\"header-buttons\">"
     if journal.editable_by?(User.current)
