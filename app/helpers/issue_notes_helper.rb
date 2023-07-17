605ded9c191f344932f4ed216c74474308814b6d
sk-ys <yskrmt@gmail.com>
2023-07-17 23:39:13 +0900

    Cleanup and format

--- app/helpers/issue_notes_helper.rb
+++ app/helpers/issue_notes_helper.rb
@@ -17,24 +17,24 @@
 
 module IssueNotesHelper
   # This program is based on QueriesHelper#column_header
-  def column_names(query, column, options={})
+  def column_names(query, column, options = {})
     if column.sortable?
       css, order = nil, column.default_order
       if column.name.to_s == query.sort_criteria.first_key
         if query.sort_criteria.first_asc?
-          css = 'sort asc icon icon-sorted-desc'
-          order = 'desc'
+          css = "sort asc icon icon-sorted-desc"
+          order = "desc"
         else
-          css = 'sort desc icon icon-sorted-asc'
-          order = 'asc'
+          css = "sort desc icon icon-sorted-asc"
+          order = "asc"
         end
       end
       param_key = options[:sort_param] || :sort
-      sort_param = {param_key => query.sort_criteria.add(column.name, order).to_param}
-      sort_param = {$1 => {$2 => sort_param.values.first}} while sort_param.keys.first.to_s =~ /^(.+)\[(.+)\]$/
+      sort_param = { param_key => query.sort_criteria.add(column.name, order).to_param }
+      sort_param = { $1 => { $2 => sort_param.values.first } } while sort_param.keys.first.to_s =~ /^(.+)\[(.+)\]$/
       link_options = {
-        :title => l(:label_sort_by, "\"#{column.caption}\""),
-        :class => css
+        title: l(:label_sort_by, "\"#{column.caption}\""),
+        class: css,
       }
       if options[:sort_link_options]
         link_options.merge! options[:sort_link_options]
@@ -42,15 +42,15 @@ module IssueNotesHelper
       content =
         link_to(
           column.caption,
-          {:params => request.query_parameters.deep_merge(sort_param)},
+          { params: request.query_parameters.deep_merge(sort_param) },
           link_options
         )
     else
       content = column.caption
     end
-    content_tag('span', content, :class => column.css_classes)
+    content_tag("span", content, class: column.css_classes)
   end
-  
+
   def _project_issue_notes_path(project, *args)
     if project
       project_issue_notes_path(project, *args)
@@ -60,53 +60,60 @@ module IssueNotesHelper
   end
 
   def render_journal_update_info_empty()
-    content_tag('span', '', :class => 'update-info empty')
+    content_tag("span", "", class: "update-info empty")
   end
 
   def render_issue_note(issue, journal)
     project = issue.project
-    indice = journal.indice || journal.issue.visible_journals_with_index.find{|j| j.id == journal.id}.indice
+    indice = journal.indice || journal.issue.visible_journals_with_index.find { |j| j.id == journal.id }.indice
 
-    content = +''
+    content = +""
     content << "<div class=\"#{journal.css_classes}\" id=\"change-#{journal.id}\">"
-    content <<   "<h4 class=\"note-header\">"
-    content <<     "<div class=\"header-text\">"
-    content <<       "<span class=\"note-id\">#{l(:field_notes)}-#{indice}</span>"
-    content <<       link_to(
-                       format_time(journal.created_on),
-                       @project.present? ?
-                       project_activity_path(@project, :from => User.current.time_to_date(journal.created_on)) :
-                       activity_path( :from => User.current.time_to_date(journal.created_on))
-                     )
-    content <<       render_private_notes_indicator(journal)
-    content <<       (respond_to?(:render_journal_update_info) ? (render_journal_update_info(journal) || render_journal_update_info_empty()) : '')
-    content <<     "</div>"
-    content <<     "<div class=\"header-buttons\">"
+    content << "<h4 class=\"note-header\">"
+    content << "<div class=\"header-text\">"
+    content << "<span class=\"note-id\">#{l(:field_notes)}-#{indice}</span>"
+    content << link_to(
+      format_time(journal.created_on),
+      @project.present? ?
+        project_activity_path(@project, from: User.current.time_to_date(journal.created_on)) :
+        activity_path(from: User.current.time_to_date(journal.created_on))
+    )
+    content << render_private_notes_indicator(journal)
+    content << (respond_to?(:render_journal_update_info) ? (render_journal_update_info(journal) || render_journal_update_info_empty()) : "")
+    content << "</div>"
+    content << "<div class=\"header-buttons\">"
     if journal.editable_by?(User.current)
       content << link_to(l(:button_edit),
                          edit_journal_path(journal),
-                         :remote => true,
-                         :method => 'get',
-                         :title => l(:button_edit),
-                         :class => 'icon-only icon-edit'
-      )
+                         remote: true,
+                         method: "get",
+                         title: l(:button_edit),
+                         class: "icon-only icon-edit")
     end
-    content <<       button_tag(
-                       '', class: "ui-icon ui-icon-arrowreturnthick-1-n pop-out",
-                       type: "button",
-                       onclick: "toggleNotesPopOutState(" +
-                        "$(this).closest(\"div.journal.has-notes\"), " +
-                        "'##{issue.id}: #{issue.subject} - #{l(:field_notes)}-#{ indice }');",
-                       title: l(:issue_note_list_label_pop_out)
-                      )
-    content <<     "</div>"
-    content <<   "</h4>"
-    content <<   "<div class=\"note-info\">"
-    content <<     l(:field_updated_by).html_safe + ": " + link_to_user(journal.user)
-    content <<   "</div>"
-    content <<   render_notes(issue, journal)
+    content << button_tag(
+      "", class: "ui-icon ui-icon-arrowreturnthick-1-n pop-out",
+          type: "button",
+          onclick: "toggleNotesPopOutState(" +
+                   "$(this).closest(\"div.journal.has-notes\"), " +
+                   "'##{issue.id}: #{issue.subject} - #{l(:field_notes)}-#{indice}');",
+          title: l(:issue_note_list_label_pop_out),
+    )
+    content << "</div>"
+    content << "</h4>"
+    content << "<div class=\"note-info\">"
+    content << l(:field_updated_by).html_safe + ": " + link_to_user(journal.user)
+    content << "</div>"
+    content << render_notes(issue, journal)
     content << "</div>"
 
     content.html_safe
+  end
+
+  def issue_row_classes(issue, level, enable_compact_mode, enable_variable_height)
+    classes = [cycle("odd", "even"), issue.css_classes]
+    classes << "idnt idnt-#{level}" if level > 0
+    classes << "collapse-row" if enable_compact_mode
+    classes << "variable-height" if enable_variable_height
+    classes.join(" ")
   end
 end
