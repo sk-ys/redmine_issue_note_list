a8259ca70e2182ce3640922721a9fbc97340c733
sk-ys <yskrmt@gmail.com>
2024-01-09 23:13:41 +0900

    Rename controller IssueNotes to IssueNoteList

--- app/controllers/issue_note_list_controller.rb
+++ app/controllers/issue_note_list_controller.rb
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-class IssueNotesController < ApplicationController
+class IssueNoteListController < ApplicationController
   unloadable
   menu_item :redmine_issue_note_list
   before_action :find_optional_project, :only => [:index]
--- lib/issue_notes/hooks.rb
+++
@@ -1,4 +0,0 @@
-class IssueNotes::Hooks < Redmine::Hook::ViewListener
-  render_on :view_journals_update_js_bottom,
-            partial: 'issue_notes/journals_update_js_bottom'
-end
