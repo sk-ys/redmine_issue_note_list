dccbabf91034646d9b9ab20bca6a62476436b1b8
sk-ys <yskrmt@gmail.com>
2024-07-16 20:04:24 +0900

    FIx cell height after double click

--- assets/javascripts/issue_note_list.js
+++ assets/javascripts/issue_note_list.js
@@ -388,25 +388,14 @@ IssueNoteList.fn = {
         alsoResize: $(this).children("td"),
         create: function () {
           const $tr = $(this);
-          $tr.children(".ui-resizable-handle").on("dblclick", () => {
+          $tr.children(".ui-resizable-handle").on("dblclick", (e) => {
             // Reset height
-            const $targets = $tr.find(
-              "> td.issue-status" +
-                ", > td.recent_notes" +
-                ", > td.block_column > div.wiki"
-            );
-
-            if (
-              $targets
-                .map((_, e) => /(^|\w)height:/.test($(e).attr("style")))
-                .toArray()
-                .includes(true)
-            ) {
-              $targets.each((_, e) => $(e).css("height", ""));
-            } else if (!$tr.hasClass("variable-height")) {
+            if (!$tr.hasClass("variable-height")) {
               $tr.addClass("variable-height");
+              IssueNoteList.fn.setNoteHeightVariable(e, true);
             } else {
               $tr.removeClass("variable-height");
+              IssueNoteList.fn.setNoteHeightVariable(e, false);
             }
           });
         },
