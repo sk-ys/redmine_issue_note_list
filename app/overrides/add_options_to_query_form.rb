6b9208fff6a3bc632216bf766c44c2360fa6cdcd
sk-ys <yskrmt@gmail.com>
2025-11-23 13:05:22 +0900

    Improve the design of the issue note list query form

--- app/overrides/add_options_to_query_form.rb
+++ app/overrides/add_options_to_query_form.rb
@@ -2,7 +2,7 @@ module AddOptionsToQueryForm
   Deface::Override.new(
     virtual_path: 'queries/_query_form',
     name: 'add_options_to_query_form',
-    insert_bottom: '#options',
+    insert_bottom: '#list-definition',
     text: "<% if controller_name == 'issue_note_list' %><%= render partial: 'issue_note_list/query_form_options' %><% end %>"
   )
 end
