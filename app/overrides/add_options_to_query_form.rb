16b18bd6a050fea6db0a81c1538214b649c4c4b1
sk-ys <yskrmt@gmail.com>
2025-11-23 11:21:51 +0900

    Move Issue Note List query options to standard query form options

---
+++ app/overrides/add_options_to_query_form.rb
@@ -0,0 +1,8 @@
+module AddOptionsToQueryForm
+  Deface::Override.new(
+    virtual_path: 'queries/_query_form',
+    name: 'add_options_to_query_form',
+    insert_bottom: '#options',
+    text: "<% if controller_name == 'issue_note_list' %><%= render partial: 'issue_note_list/query_form_options' %><% end %>"
+  )
+end
