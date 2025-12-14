33a1242c5734ff9558aaf8966fe4653c587664da
sk-ys <yskrmt@gmail.com>
2025-12-14 23:28:04 +0900

    v0.1.0

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_issue_note_list do
   name "Redmine Issue Note List plugin"
   author "sk-ys"
   description "This is a plugin for Redmine"
-  version "0.0.1"
+  version "0.1.0"
   url "https://github.com/sk-ys/redmine_issue_note_list"
   author_url "https://github.com/sk-ys"
 
